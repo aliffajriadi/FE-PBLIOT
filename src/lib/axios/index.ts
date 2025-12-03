6e7338aff418f485c5715f3b47b0d7e129c1324b
aliffajriadi <aliffajriadi@gmail.com>
2025-12-04 01:27:49 +0700

    fix env

--- src/lib/axios/index.ts
+++ src/lib/axios/index.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: 'http://192.168.1.10:3000/api', // tinggal ganti di .env
+  baseURL: process.env.NEXT_PUBLIC_API_URL, // tinggal ganti di .env
   withCredentials: true, // kalau pakai cookie
 });
