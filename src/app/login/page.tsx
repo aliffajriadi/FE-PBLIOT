833e0ad1cc07938dcc8d228cf8afe0544dfda247
aliffajriadi <aliffajriadi@gmail.com>
2025-12-05 16:54:34 +0700

    fix client handling component

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,36 +1,10 @@
-"use client";
-export const dynamic = "force-dynamic";
+import { Suspense } from "react";
+import LoginClient from "./component/LoginClient";
 
-import LoginForm from "./component/FormLogin";
-import { useSearchParams } from "next/navigation";
-import { useEffect } from "react";
-import { toast } from "sonner";
-
-const LoginPage = () => {
-  const params = useSearchParams();
-  const error = params.get("error");
-
-  useEffect(() => {
-    if (!error) return;
-    setTimeout(() => {
-      if (error === "unauthorized") toast.error("Anda tidak memiliki akses");
-      if (error === "unauth") toast.error("Anda belum login");
-    }, 50);
-  }, [error]);
-
+export default function LoginPage() {
   return (
-    <div className="min-h-screen bg-gradient-to-br from-blue-100 via-blue-50 to-indigo-100 flex flex-col">
-      <div className="flex-1 flex items-center justify-center py-12">
-        <LoginForm />
-      </div>
-
-      <footer className="text-center pb-8">
-        <p className="text-[#2F4C87] font-semibold text-lg">
-          Smart System for School Attendance Needs.
-        </p>
-      </footer>
-    </div>
+    <Suspense fallback={<div>Loading...</div>}>
+      <LoginClient />
+    </Suspense>
   );
-};
-
-export default LoginPage;
+}
